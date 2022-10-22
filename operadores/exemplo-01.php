b854efddd2b922dccabe7641c51ec13f5925a5d3
Lukas132ss <lk@serrante.com.br>
2022-10-22 16:20:56 -0300

    exemplos de operadores no php

---
+++ operadores/exemplo-01.php
@@ -0,0 +1,9 @@
+<?php
+
+$nome = "Lukas";
+
+echo $nome . " mais alguma coisa <br/>";
+
+$nome .= " Treinamentos";
+
+echo $nome;
---
+++ operadores/exemplo-03.php
@@ -0,0 +1,28 @@
+<?php
+
+$a = 10;
+$b = 2;
+
+echo $a + $b." soma";
+
+echo "<br>";
+
+echo $a - $b." subtração";
+
+echo "<br>";
+
+echo $a * $b." multiplicação";
+
+echo "<br>";
+
+echo $a / $b." divisão";
+
+echo "<br>";
+
+echo $a % $b." resto do produto";
+
+echo "<br>";
+
+echo $a ** $b." ao quadrado";
+
+echo "<br>";
