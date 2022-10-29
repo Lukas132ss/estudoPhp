ceba450fac01b25ebe44b5f1b99e85c310a95fe6
Lukas132ss <lk@serrante.com.br>
2022-10-29 15:46:54 -0300

    adicionando comandos

--- DAO/index.php
+++ DAO/index.php
@@ -2,8 +2,37 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+// Carrega um usuáro
+// $root = new Usuario();
+// $root->loadById(3);
+// echo $root;
 
-$root->loadById(3);
+// $lista = Usuario::getList();
+// echo json_encode($lista);
 
-echo $root;
+// // Carrega uma lista de usuários buscando pelo login
+// $search = Usuario::search("Lu");
+// echo json_encode($search);
+
+// Carrega um usuário usando o login e a senha
+// $usuario = new Usuario();
+// $usuario->login("root","ABCDE");
+// echo $usuario;
+
+// Criando um novo usuário
+// $aluno = new Usuario("aluno", "@lun0");
+// $aluno->insert();
+// echo $aluno;
+
+// Alterar um usuário
+// $usuario = new Usuario();
+// $usuario->loadById(11);
+// $usuario->update("professor", "prof123");
+// echo $usuario;
+
+// Deletar um usuário
+$usuario = new Usuario();
+$usuario->loadById(1);
+$usuario->delete();
+
+echo $usuario;
